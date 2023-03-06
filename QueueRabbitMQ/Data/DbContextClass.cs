480c1a35bf87c724b88b2aa2b6570837303345e5
Piriyanka Periyathamby <80697383+Piriyanka18@users.noreply.github.com>
2023-03-06 09:57:21 +0530

    Add files via upload

---
+++ QueueRabbitMQ/Data/DbContextClass.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using QueueRabbitMQ.Models;
+
+namespace QueueRabbitMQ.Data
+{
+    public class DbContextClass : DbContext
+    {
+
+        public DbContextClass(DbContextOptions<DbContextClass> options) : base(options)
+        {
+        }
+
+       
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
